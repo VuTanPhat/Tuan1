1c49174e78d61369a37f3c2c49d0f23eaab1dae2
VuTanPhat <124854669+VuTanPhat@users.noreply.github.com>
2023-04-18 10:42:02 +0700

    Add files via upload

--- main.java
+++ main.java
@@ -1,36 +1,33 @@
-package Qlx;
+package ThucPham;
 
 import java.util.Scanner;
-
 public class main {
+    private static Scanner scanner;
 
 	public static void main(String[] args) {
-		danhsach ds=new danhsach();
-		Vehicle b=new Vehicle();
-		int n = 3;
-		
-		Scanner sc=new Scanner(System.in);
-		do 
-		{
-			ds.menu();
-			System.out.print("Mời chọn: ");
-			n=sc.nextInt();
-			switch(n) {
-			case 1:{
-				ds.them(b);
-				
-				break;
-			}
-			case 2:{
-				ds.xuat();
-				break;
-			}
-			case 3:{
-				break;
-			}
-			}
-		}while(n!=3);
-		
-	}
+        scanner = new Scanner(System.in);
+        HangThucPham tp = new HangThucPham();
+        boolean kt = true;
+        boolean th = true;
+        System.out.println("Nhập mã hàng : ");
+        tp.setMaHang(scanner.nextInt());
+        scanner.nextLine();
+        do {
+            System.out.println("Nhập tên hàng : ");
+            tp.setTenHang(scanner.nextLine());
+        } while (tp.kiemTraTenHang(kt));
+        System.out.println("Nhập đơn giá : ");
+        tp.setDonGia(scanner.nextDouble());
+        do {
+            System.out.println("Nhập năm, tháng, ngày sản xuất: ");
+            tp.setNSX(scanner.nextInt(), scanner.nextInt(), scanner.nextInt());
+            System.out.println("nhap năm,tháng,ngày hết hạn : ");
+            tp.setHSD(scanner.nextInt(), scanner.nextInt(), scanner.nextInt());
+        } while (tp.kiemTraNgay(th));
+        System.out.println(tp);
+        tp.kiemTraHSD();
+    }
 
+    private main() {
+    }
 }
